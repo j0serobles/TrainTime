0551a4161089048d0c09ac7c0ebd524f1190e139
joraro <engjoserobles@gmail.com>
2019-07-20 23:27:50 -0400

    Saving Trains to Firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,6 +9,7 @@ $( document ).ready(function() {
             messagingSenderId: "481457808049",
             appId: "1:481457808049:web:9f8561538539db74"
         };
+
         firebase.initializeApp(config);
 
         // Assign the reference to the database to a variable named 'database'
@@ -21,44 +22,71 @@ $( document ).ready(function() {
         var trainsRef = database.ref("/trains");
 
 
-
+        ///////////////////////////////////////////////////////////////////////
         // At the page load and subsequent value changes, get a snapshot of the local data.
         // This callback allows the page to stay updated with the values in firebase node "trains"
-        database.ref("/trains").on("value", function(snapshot) {
-        
-            // Print the local data to the console.
-        console.log(snapshot.val());
+        database.ref("/trains").on("child_added", function(snapshot) {
+            // Print the local data to the console.  Will print NULL the first time.
+            console.log("child_added event received: Snapshot:" + snapshot.val());
 
+            if (snapshot.val() !== null) {
+                console.log("child_added event received: Snapshot:" + JSON.stringify(snapshot.val()));
+                console.log("train name is:" + snapshot.val().trainName );
+                //Create the row element for the trains table
+                var tr = $("<tr>"); 
+                //Create a <td> element for each data field from the snapshot
+                var trainNumber      = $("<td>").text(snapshot.val().trainNumber); 
+                var trainName        = $("<td>").text(snapshot.val().trainName); 
+                var trainDestination = $("<td>").text(snapshot.val().trainDestination); 
+                var trainFirstTime   = $("<td>").text(snapshot.val().trainFirstTime);
+                var trainFrequency   = $("<td>").text(snapshot.val().trainFrequency);
+                var trainNextArrival = $("<td>").text(""); 
+                var trainMinsAway    = $("<td>").text(""); 
+
+                // var monthsWorked = moment().diff( moment( snapshot.val().empStartDate,"MM/DD/YYYY" ), 'months'); 
+                //Append the <td>'s to the <tr>
+                tr.append(trainNumber).append(trainName).append(trainDestination).append(trainFrequency).append(trainNextArrival).append(trainMinsAway);
+                //Append the row to the <tbody>
+                $("#train-tbody").append(tr);
+            }
         }); 
 
-
-
+        ///////////////////////////////////////////////////////////////////////
         // Whenever a user clicks the "Submit" button in the form
         $("#submit-train").on("click", function(event) {
-
-            event.preventDefault();
-        
-            // Get the input values
-            var trainNumber      = $("#trainNumber").val().trim();
-            var trainName        = $("#trainName").val().trim();
-            var trainDestination = $("#trainDestination").val().trim();
-            var trainFirstTime   = $("#trainFirstTime").val().trim();
-            var trainFrequency   = $("#trainFrequency").val().trim();
-        
-            // Log the Bidder and Price (Even if not the highest)
-            console.log ("train number = " + trainNumber + ", train name = " + trainName  + ",destination " 
-            +  trainDestination + ", first time = "  + trainFirstTime + ", frequency = "  + trainFrequency ); 
-        
-            // Save the new price in Firebase
-            //   database.ref("/bidderData").set({
-            //     highBidder: bidderName,
-            //     highPrice: bidderPrice
-            //   });
-        
-            // Add the train to the /trains collection. 
-
+          event.preventDefault();
+          //Create the object to be pushed to the DB
+          var newTrain = {
+              trainNumber      : $("#trainNumber").val().trim(),
+              trainName        : $("#trainName").val().trim(),
+              trainDestination : $("#trainDestination").val().trim(),
+              trainFirstTime   : $("#trainFirstTime").val().trim(),
+              trainFrequency   : $("#trainFrequency").val().trim()
+          }
+          // Log the Bidder and Price (Even if not the highest)
+          console.log ("train number = " + newTrain.trainNumber + ", train name = " + newTrain.trainName  + ",destination " 
+          +  newTrain.trainDestination + ", first time = "  + newTrain.trainFirstTime + ", frequency = "  + newTrain.trainFrequency ); 
+          // Save the new Train in Firebase
+          database.ref("/trains").push(newTrain); 
         });
 
+        ///////////////////////////////////////////////////////////////////////////////
+        $("#details-collapse-button").on("click", function() {
+            
+            if ( $(this).attr("aria-expanded") === "true") {
+                
+                //Area was collapsed and has been expanded.  Change icon to "-" 
+                $("#link-text-icon").removeClass("fa-minus-square");
+                $("#link-text-icon").addClass("fa-plus-square");
+            }
+            else if ( $(this).attr("aria-expanded") === "false") {
+                
+                //Area was expanded and has been collapsed.  Change icon to "+" 
+                $("#link-text-icon").removeClass("fa-plus-square");
+                $("#link-text-icon").addClass("fa-minus-square");
+            }
+        });
 
-  console.log( "ready!" );
-});
+        console.log( "ready!" );
+
+    });
